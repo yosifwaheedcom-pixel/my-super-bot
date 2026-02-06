0c75a0be0aea174d9d0480f8759050210881f607
yosifwaheedcom-pixel <yosifwaheed.com@gmail.com>
2026-02-06 13:05:05 +0300

    pro_bot.py

--- pro_bot.py
+++ pro_bot.py
@@ -26,7 +26,7 @@ threading.Thread(target=start_server, daemon=True).start()
 # ==========================================
 # ⚙️ الإعدادات المتقدمة (Config)
 # ====================================
-TOKEN = "8298277087:AAF7Z6J24bIvFb1e_xpZvSuJm1mXhZocJzw"
+TOKEN = "8298277087:AAEv36igY-juy9TAIJHDvXwqx4k7pMF3qPM"
 VERIFICATION_CODE = "4415"
 QURAN_VIDEO_URL = "https://www.instagram.com/reel/DUYAQBaihUg/?igsh=Y2dhNDNuMGRiYWp3"
 
